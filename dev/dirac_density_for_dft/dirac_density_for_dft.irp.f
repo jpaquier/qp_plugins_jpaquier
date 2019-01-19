71f627c8091eeebd436852f4fae13a967a5b848c
jpaquier <jpaquier@lct.jussieu.fr>
2019-01-19 17:19:40 +0100

    QP compiling. Just need to make sure it still works truly

--- dev/dirac_density_for_dft/dirac_density_for_dft.irp.f
+++ dev/dirac_density_for_dft/dirac_density_for_dft.irp.f
@@ -1,4 +1,4 @@
- BEGIN_PROVIDER [complex*16,dirac_mo_coef_electronic_dft_dft, (2*dirac_ao_num,elec_num)]
+ BEGIN_PROVIDER [complex*16,dirac_mo_coef_electronic_dft, (2*dirac_ao_num,elec_num)]
  implicit none
  BEGIN_DOC
  !This is the matrix of the eigenvectors of the Fock matrix 
@@ -19,12 +19,12 @@
    ! C.C* 
    ! with C the dirac_mo_coef_electronic_dft 
    END_DOC
-   complex*16, Allocatable    :: dirac_one_body_dm_ao_for_dft_ao_tmp(:,:,:)
-   Allocate (dirac_one_body_dm_ao_for_dft_ao_tmp(2*dirac_ao_num,2*dirac_ao_num,N_states))
+   complex*16, Allocatable    :: dirac_one_body_dm_ao_for_dft_tmp(:,:,:)
+   Allocate (dirac_one_body_dm_ao_for_dft_tmp(2*dirac_ao_num,2*dirac_ao_num,N_states))
    call zgemm('N','C',2*dirac_ao_num,2*dirac_ao_num,elec_num,(1.d0,0.d0), &
         dirac_mo_coef_electronic_dft, size(dirac_mo_coef_electronic_dft,1), &
         dirac_mo_coef_electronic_dft, size(dirac_mo_coef_electronic_dft,1), (0.d0,0.d0), &
-        dirac_one_body_dm_ao_for_dft_ao_tmp, size(dirac_one_body_dm_ao_for_dft_tmp,1))
+        dirac_one_body_dm_ao_for_dft_tmp, size(dirac_one_body_dm_ao_for_dft_tmp,1))
   dirac_one_body_dm_ao_for_dft = (dirac_one_body_dm_ao_for_dft_tmp)
   deallocate(dirac_one_body_dm_ao_for_dft_tmp)
  END_PROVIDER
