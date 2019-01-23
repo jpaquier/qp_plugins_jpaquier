2e0f7616f48e3ca8c01d960386d1393cff6de868
Julien Paquier <jpaquier@noumea.lct.jussieu.fr>
2019-01-23 15:33:15 +0100

    Knowles alpha coefficient extended

--- dev/dirac_ks_rs/dirac_ks_rs.main.irp.f
+++ dev/dirac_ks_rs/dirac_ks_rs.main.irp.f
@@ -2,26 +2,13 @@ program dirac_ks_rs
  implicit none
  integer :: istate,i,j
  double precision :: r(3)
- double precision :: mu,weight
- double precision :: e_x,v_x,ex_ab,vx_a,vx_b
- double precision, allocatable :: rho(:), rhoa(:),rhob(:)
- allocate(rho(N_states), rhoa(N_states),rhob(N_states))
- do istate = 1, N_states
-  do i = 1, n_points_final_grid
-   r(1) = final_grid_points(1,i)
-   r(2) = final_grid_points(2,i)
-   r(3) = final_grid_points(3,i)
-   weight=final_weight_at_r_vector(i)
-   rho(istate) = dirac_one_body_dm_at_r(i,istate)
-   call dirac_ex_LDA_sr(mu_erf,rho(istate),e_x,v_x)
-   print*,r(1),r(2),r(3), rho(istate)
+ double precision :: mu,weight,x
  
-  !rhoa(istate) = one_e_dm_alpha_at_r(i,istate)
-  !rhob(istate) = one_e_dm_beta_at_r(i,istate)
-  !call ex_LDA_sr(mu_erf,rhoa(istate),rhob(istate),ex_ab,vx_a,vx_b)
-  !print*,r(1),r(2),r(3), rhoa(istate), rhob(istate)
- 
-  enddo
- enddo
-
+!do i = 36,54
+! do j = 1, n_points_radial_grid-1
+! !x = grid_points_radial(j)
+!   
+! !print*, i,x,m_knwoles, knowles_function(alpha_knowles(i),m_knowles,x) 
+! enddo
+!enddo
 end
