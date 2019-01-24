0bd6887451ee44cd8a47bb3a0cb10b1d4d7d1706
jpaquier <jpaquier@lct.jussieu.fr>
2019-01-24 22:19:18 +0100

    All .dat okay, now is the time to GNUUUUUUUUUU

--- dev/dirac_ks_rs/dirac_exchange_dhf_mu.irp.f
+++ dev/dirac_ks_rs/dirac_exchange_dhf_mu.irp.f
@@ -88,19 +88,19 @@ program dirac_exchange_dhf_mu
  !endif
 
  !For Krypton
-  if (mu_erf .lt. 10) then
-   mu_erf += 1d0
-  elseif (mu_erf .lt. 40) then
-   mu_erf+=2.d0
-  elseif (mu_erf .lt. 200) then
-   mu_erf+=10.d0
-  elseif (mu_erf .lt. 400) then
-   mu_erf+=20.0d0
-  elseif (mu_erf .lt. 1200) then
-   mu_erf+=40.d0
-  else 
-   mu_erf+=80.d0
-  endif
+ !if (mu_erf .lt. 10) then
+ ! mu_erf += 1d0
+ !elseif (mu_erf .lt. 40) then
+ ! mu_erf+=2.d0
+ !elseif (mu_erf .lt. 200) then
+ ! mu_erf+=10.d0
+ !elseif (mu_erf .lt. 400) then
+ ! mu_erf+=20.0d0
+ !elseif (mu_erf .lt. 1200) then
+ ! mu_erf+=40.d0
+ !else 
+ ! mu_erf+=80.d0
+ !endif
 
  !For Xenon
  !if (mu_erf .lt. 20) then
@@ -133,19 +133,19 @@ program dirac_exchange_dhf_mu
  !endif
 
  !For Oganesson
- !if (mu_erf .lt. 80) then
- ! mu_erf += 8d0
- !elseif (mu_erf .lt. 320) then
- ! mu_erf+=16.d0
- !elseif (mu_erf .lt. 1600) then
- ! mu_erf+=80.d0
- !elseif (mu_erf .lt. 3200) then
- ! mu_erf+=160.0d0
- !elseif (mu_erf .lt. 9600) then
- ! mu_erf+=320.d0
- !else
- ! mu_erf+=640.d0
- !endif
+  if (mu_erf .lt. 80) then
+   mu_erf += 8d0
+  elseif (mu_erf .lt. 320) then
+   mu_erf+=16.d0
+  elseif (mu_erf .lt. 1600) then
+   mu_erf+=80.d0
+  elseif (mu_erf .lt. 3200) then
+   mu_erf+=160.0d0
+  elseif (mu_erf .lt. 9600) then
+   mu_erf+=320.d0
+  else
+   mu_erf+=640.d0
+  endif
  
   call ezfio_set_ao_two_e_erf_ints_mu_erf(mu_erf)
 
