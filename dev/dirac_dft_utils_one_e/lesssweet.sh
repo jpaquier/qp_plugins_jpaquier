35575e573580cf74c01f836dd30d902b486ec729
jpaquier <jpaquier@lct.jussieu.fr>
2019-02-05 23:28:39 +0100

    The potential of the Pade Order 4 seems fine, some more tests would be needed

--- dev/dirac_dft_utils_one_e/lesssweet.sh
+++ dev/dirac_dft_utils_one_e/lesssweet.sh
@@ -1,91 +1,159 @@
 #!/bin/bash
 
- sed -i '1,229s/2.718281828459045\*\*/dexp/g' test.txt
- sed -i '1,229s/Log/dlog/g' test.txt
- sed -i '1,229s/Sqrt/dsqrt/g' test.txt
+ sed -i '1,500s/e-8/d-8p/g' test.txt
+ sed -i '1,500s/e2/d+2p/g' test.txt
+ sed -i '1,500s/e3/d+3p/g' test.txt
+ sed -i '1,500s/e4/d+4p/g' test.txt
+ sed -i '1,500s/e5/d+5p/g' test.txt
+ sed -i '1,500s/e6/d+6p/g' test.txt
+ sed -i '1,500s/e7/d+7p/g' test.txt
+ sed -i '1,500s/e8/d+8p/g' test.txt
+ sed -i '1,500s/e9/d+9p/g' test.txt
+ sed -i '1,500s/e10/d+10p/g' test.txt
+ 
+ sed -i '1,500s/2.718281828459045\*\*/dexp/g' test.txt
+ sed -i '1,500s/Erf/derf/g' test.txt
+ sed -i '1,500s/Log/dlog/g' test.txt
+ sed -i '1,500s/Sqrt/dsqrt/g' test.txt
 
- sed -i '1,229s/0\ /0d0 /g' test.txt
- sed -i '1,229s/1\ /1d0 /g' test.txt
- sed -i '1,229s/2\ /2d0 /g' test.txt
- sed -i '1,229s/3\ /3d0 /g' test.txt
- sed -i '1,229s/4\ /4d0 /g' test.txt
- sed -i '1,229s/5\ /5d0 /g' test.txt
- sed -i '1,229s/6\ /6d0 /g' test.txt
- sed -i '1,229s/7\ /7d0 /g' test.txt
- sed -i '1,229s/8\ /8d0 /g' test.txt
- sed -i '1,229s/9\ /9d0 /g' test.txt
+ sed -i '1,500s/0\ /0d0 /g' test.txt
+ sed -i '1,500s/1\ /1d0 /g' test.txt
+ sed -i '1,500s/2\ /2d0 /g' test.txt
+ sed -i '1,500s/3\ /3d0 /g' test.txt
+ sed -i '1,500s/4\ /4d0 /g' test.txt
+ sed -i '1,500s/5\ /5d0 /g' test.txt
+ sed -i '1,500s/6\ /6d0 /g' test.txt
+ sed -i '1,500s/7\ /7d0 /g' test.txt
+ sed -i '1,500s/8\ /8d0 /g' test.txt
+ sed -i '1,500s/9\ /9d0 /g' test.txt
 
- sed -i '1,229s/0\*/0d0*/g' test.txt
- sed -i '1,229s/1\*/1d0*/g' test.txt
- sed -i '1,229s/2\*/2d0*/g' test.txt
- sed -i '1,229s/3\*/3d0*/g' test.txt
- sed -i '1,229s/4\*/4d0*/g' test.txt
- sed -i '1,229s/5\*/5d0*/g' test.txt
- sed -i '1,229s/6\*/6d0*/g' test.txt
- sed -i '1,229s/7\*/7d0*/g' test.txt
- sed -i '1,229s/8\*/8d0*/g' test.txt
- sed -i '1,229s/9\*/9d0*/g' test.txt
- sed -i '1,229s/\*\*2d0/\*\*2/g' test.txt
- sed -i '1,229s/\*\*3d0/\*\*3/g' test.txt
- sed -i '1,229s/\*\*4d0/\*\*4/g' test.txt
- sed -i '1,229s/\*\*5d0/\*\*5/g' test.txt
- sed -i '1,229s/\*\*6d0/\*\*6/g' test.txt
- sed -i '1,229s/\*\*7d0/\*\*7/g' test.txt
- sed -i '1,229s/\*\*8d0/\*\*8/g' test.txt
- sed -i '1,229s/\*\*9d0/\*\*9/g' test.txt
-
- sed -i '1,229s/0\.\ /0.d0 /g' test.txt
- sed -i '1,229s/0\.\*/0.d0*/g' test.txt
- sed -i '1,229s/1\.\ /1.d0 /g' test.txt
- sed -i '1,229s/1\.\*/1.d0*/g' test.txt
- sed -i '1,229s/2\.\ /2.d0 /g' test.txt
- sed -i '1,229s/2\.\*/2.d0*/g' test.txt
- sed -i '1,229s/3\.\ /3.d0 /g' test.txt
- sed -i '1,229s/3\.\*/3.d0*/g' test.txt
- sed -i '1,229s/4\.\ /4.d0 /g' test.txt
- sed -i '1,229s/4\.\*/4.d0*/g' test.txt
- sed -i '1,229s/5\.\ /5.d0 /g' test.txt
- sed -i '1,229s/5\.\*/5.d0*/g' test.txt
- sed -i '1,229s/6\.\ /6.d0 /g' test.txt
- sed -i '1,229s/6\.\*/6.d0*/g' test.txt
- sed -i '1,229s/7\.\ /7.d0 /g' test.txt
- sed -i '1,229s/7\.\*/7.d0*/g' test.txt
- sed -i '1,229s/8\.\ /8.d0 /g' test.txt
- sed -i '1,229s/8\.\*/8.d0*/g' test.txt
- sed -i '1,229s/9\.\ /9.d0 /g' test.txt
- sed -i '1,229s/9\.\*/9.d0*/g' test.txt
-
-
- sed -i '1,229s/ 1\// 1.d0\//g' test.txt
- sed -i '1,229s/ 2\// 2.d0\//g' test.txt
- sed -i '1,229s/ 3\// 3.d0\//g' test.txt
- sed -i '1,229s/ 4\// 4.d0\//g' test.txt
- sed -i '1,229s/ 5\// 5.d0\//g' test.txt
- sed -i '1,229s/ 6\// 6.d0\//g' test.txt
- sed -i '1,229s/ 7\// 7.d0\//g' test.txt
- sed -i '1,229s/ 8\// 8.d0\//g' test.txt
- sed -i '1,229s/ 9\// 9.d0\//g' test.txt
+ sed -i '1,500s/0\*/0d0*/g' test.txt
+ sed -i '1,500s/1\*/1d0*/g' test.txt
+ sed -i '1,500s/2\*/2d0*/g' test.txt
+ sed -i '1,500s/3\*/3d0*/g' test.txt
+ sed -i '1,500s/4\*/4d0*/g' test.txt
+ sed -i '1,500s/5\*/5d0*/g' test.txt
+ sed -i '1,500s/6\*/6d0*/g' test.txt
+ sed -i '1,500s/7\*/7d0*/g' test.txt
+ sed -i '1,500s/8\*/8d0*/g' test.txt
+ sed -i '1,500s/9\*/9d0*/g' test.txt
+ sed -i '1,500s/\*\*2d0/\*\*2/g' test.txt
+ sed -i '1,500s/\*\*3d0/\*\*3/g' test.txt
+ sed -i '1,500s/\*\*4d0/\*\*4/g' test.txt
+ sed -i '1,500s/\*\*5d0/\*\*5/g' test.txt
+ sed -i '1,500s/\*\*6d0/\*\*6/g' test.txt
+ sed -i '1,500s/\*\*7d0/\*\*7/g' test.txt
+ sed -i '1,500s/\*\*8d0/\*\*8/g' test.txt
+ sed -i '1,500s/\*\*9d0/\*\*9/g' test.txt
+ sed -i '1,500s/\*\*10d0/\*\*10/g' test.txt
+ sed -i '1,500s/\*\*11d0/\*\*11/g' test.txt
+ sed -i '1,500s/\*\*12d0/\*\*12/g' test.txt
+ sed -i '1,500s/\*\*13d0/\*\*13/g' test.txt
+ sed -i '1,500s/\*\*14d0/\*\*14/g' test.txt
+ sed -i '1,500s/\*\*15d0/\*\*15/g' test.txt
+ sed -i '1,500s/\*\*16d0/\*\*16/g' test.txt
+ sed -i '1,500s/\*\*17d0/\*\*17/g' test.txt
+ sed -i '1,500s/\*\*18d0/\*\*18/g' test.txt
+ sed -i '1,500s/\*\*19d0/\*\*19/g' test.txt
+ sed -i '1,500s/\*\*20d0/\*\*20/g' test.txt
 
 
- sed -i '1,229s/kF\*\*4/kF_4/g' test.txt
- sed -i '1,229s/tmpc\*\*8/tmp_c_8/g' test.txt
- sed -i '1,229s/tmpc\*\*7/tmp_c_7/g' test.txt
- sed -i '1,229s/tmpc\*\*6/tmp_c_6/g' test.txt
- sed -i '1,229s/tmpc\*\*5/tmp_c_5/g' test.txt
- sed -i '1,229s/tmpc\*\*4/tmp_c_4/g' test.txt
- sed -i '1,229s/tmpc\*\*3/tmp_c_3/g' test.txt
- sed -i '1,229s/tmpc\*\*2/tmp_c_2/g' test.txt 
- sed -i '1,229s/tmpc\*\*(-2)/tmp_c_m_2/g' test.txt 
- sed -i '1,229s/tmpc/tmp_c/g' test.txt
- sed -i '1,229s/tmpmu\*\*6/tmp_mu_6/g' test.txt
- sed -i '1,229s/tmpmu\*\*5/tmp_mu_5/g' test.txt
- sed -i '1,229s/tmpmu\*\*4/tmp_mu_4/g' test.txt
- sed -i '1,229s/tmpmu\*\*3/tmp_mu_3/g' test.txt
- sed -i '1,229s/tmpmu\*\*2/tmp_mu_2/g' test.txt
- sed -i '1,229s/tmpmu/tmp_mu/g' test.txt
+
+
+ sed -i '1,500s/0\.\ /0.d0 /g' test.txt
+ sed -i '1,500s/0\.\*/0.d0*/g' test.txt
+ sed -i '1,500s/1\.\ /1.d0 /g' test.txt
+ sed -i '1,500s/1\.\*/1.d0*/g' test.txt
+ sed -i '1,500s/1\.\//1.d0\//g' test.txt
+ sed -i '1,500s/2\.\ /2.d0 /g' test.txt
+ sed -i '1,500s/2\.\*/2.d0*/g' test.txt
+ sed -i '1,500s/2\.\//2.d0\//g' test.txt
+ sed -i '1,500s/3\.\ /3.d0 /g' test.txt
+ sed -i '1,500s/3\.\*/3.d0*/g' test.txt
+ sed -i '1,500s/3\.\//3.d0\//g' test.txt
+ sed -i '1,500s/4\.\ /4.d0 /g' test.txt
+ sed -i '1,500s/4\.\*/4.d0*/g' test.txt
+ sed -i '1,500s/4\.\//4.d0\//g' test.txt
+ sed -i '1,500s/5\.\ /5.d0 /g' test.txt
+ sed -i '1,500s/5\.\*/5.d0*/g' test.txt
+ sed -i '1,500s/5\.\//5.d0\//g' test.txt
+ sed -i '1,500s/6\.\ /6.d0 /g' test.txt
+ sed -i '1,500s/6\.\*/6.d0*/g' test.txt
+ sed -i '1,500s/6\.\//6.d0\//g' test.txt
+ sed -i '1,500s/7\.\ /7.d0 /g' test.txt
+ sed -i '1,500s/7\.\*/7.d0*/g' test.txt
+ sed -i '1,500s/7\.\//7.d0\//g' test.txt
+ sed -i '1,500s/8\.\ /8.d0 /g' test.txt
+ sed -i '1,500s/8\.\*/8.d0*/g' test.txt
+ sed -i '1,500s/8\.\//8.d0\//g' test.txt
+ sed -i '1,500s/9\.\ /9.d0 /g' test.txt
+ sed -i '1,500s/9\.\*/9.d0*/g' test.txt
+ sed -i '1,500s/9\.\//9.d0\//g' test.txt
+
+ sed -i '1,500s/ 1\// 1.d0\//g' test.txt
+ sed -i '1,500s/1\//1.d0\//g' test.txt
+ sed -i '1,500s/ 2\// 2.d0\//g' test.txt
+ sed -i '1,500s/2\//2.d0\//g' test.txt
+ sed -i '1,500s/ 3\// 3.d0\//g' test.txt
+ sed -i '1,500s/3\//3.d0\//g' test.txt
+ sed -i '1,500s/ 4\// 4.d0\//g' test.txt
+ sed -i '1,500s/4\//4.d0\//g' test.txt
+ sed -i '1,500s/ 5\// 5.d0\//g' test.txt
+ sed -i '1,500s/5\//5.d0\//g' test.txt
+ sed -i '1,500s/ 6\// 6.d0\//g' test.txt
+ sed -i '1,500s/6\//6.d0\//g' test.txt
+ sed -i '1,500s/ 7\// 7.d0\//g' test.txt
+ sed -i '1,500s/7\//7.d0\//g' test.txt
+ sed -i '1,500s/ 8\// 8.d0\//g' test.txt
+ sed -i '1,500s/8\//8.d0\//g' test.txt
+ sed -i '1,500s/ 9\// 9.d0\//g' test.txt
+ sed -i '1,500s/9\//9.d0\//g' test.txt
+
+
+ sed -i '1,500s/kF\*\*4/kF_4/g' test.txt
+ sed -i '1,500s/tmpc\*\*8/tmp_c_8/g' test.txt
+ sed -i '1,500s/tmpc\*\*7/tmp_c_7/g' test.txt
+ sed -i '1,500s/tmpc\*\*6/tmp_c_6/g' test.txt
+ sed -i '1,500s/tmpc\*\*5/tmp_c_5/g' test.txt
+ sed -i '1,500s/tmpc\*\*4/tmp_c_4/g' test.txt
+ sed -i '1,500s/tmpc\*\*3/tmp_c_3/g' test.txt
+ sed -i '1,500s/tmpc\*\*2/tmp_c_2/g' test.txt 
+ sed -i '1,500s/tmpc\*\*(-2)/tmp_c_m_2/g' test.txt 
+ sed -i '1,500s/tmpc/tmp_c/g' test.txt
+
+ sed -i '1,500s/tmpmu\*\*16/tmp_mu_16/g' test.txt
+ sed -i '1,500s/tmpmu\*\*15/tmp_mu_15/g' test.txt
+ sed -i '1,500s/tmpmu\*\*14/tmp_mu_14/g' test.txt
+ sed -i '1,500s/tmpmu\*\*13/tmp_mu_13/g' test.txt
+ sed -i '1,500s/tmpmu\*\*12/tmp_mu_12/g' test.txt
+ sed -i '1,500s/tmpmu\*\*11/tmp_mu_11/g' test.txt
+ sed -i '1,500s/tmpmu\*\*10/tmp_mu_10/g' test.txt
+ sed -i '1,500s/tmpmu\*\*9/tmp_mu_9/g' test.txt
+ sed -i '1,500s/tmpmu\*\*8/tmp_mu_8/g' test.txt
+ sed -i '1,500s/tmpmu\*\*7/tmp_mu_7/g' test.txt
+ sed -i '1,500s/tmpmu\*\*6/tmp_mu_6/g' test.txt
+ sed -i '1,500s/tmpmu\*\*5/tmp_mu_5/g' test.txt
+ sed -i '1,500s/tmpmu\*\*4/tmp_mu_4/g' test.txt
+ sed -i '1,500s/tmpmu\*\*3/tmp_mu_3/g' test.txt
+ sed -i '1,500s/tmpmu\*\*2/tmp_mu_2/g' test.txt
+ sed -i '1,500s/tmpmu/tmp_mu/g' test.txt
   
+ sed -i '1,500s/d-8p/d-8/g' test.txt
+ sed -i '1,500s/d+2p/d+2/g' test.txt
+ sed -i '1,500s/d+3p/d+3/g' test.txt
+ sed -i '1,500s/d+4p/d+4/g' test.txt
+ sed -i '1,500s/d+5p/d+5/g' test.txt
+ sed -i '1,500s/d+6p/d+6/g' test.txt
+ sed -i '1,500s/d+7p/d+7/g' test.txt
+ sed -i '1,500s/d+8p/d+8/g' test.txt
+ sed -i '1,500s/d+9p/d+9/g' test.txt
+ sed -i '1,500s/d+10p/d+10/g' test.txt
   
-  
+ sed -i '1,500s/dexp(1\.d0/dexp(z1/g' test.txt
+ sed -i '1,500s/dexp(2\.d0/dexp(z2/g' test.txt
+ sed -i '1,500s/dexp(3\.d0/dexp(z3/g' test.txt
+ sed -i '1,500s/derf(1\.d0/derf(z1/g' test.txt
+ 
   
   
   
