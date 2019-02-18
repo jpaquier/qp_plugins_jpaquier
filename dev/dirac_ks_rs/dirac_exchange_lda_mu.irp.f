0d60e17c80f7fbb1f4d80978321a14a3d94d991f
Julien Paquier <jpaquier@noumea.lct.jussieu.fr>
2019-02-18 15:11:58 +0100

    10e incoming

--- dev/dirac_ks_rs/dirac_exchange_lda_mu.irp.f
+++ dev/dirac_ks_rs/dirac_exchange_lda_mu.irp.f
@@ -109,34 +109,34 @@ program dirac_exchange_lda_mu
  !endif
 
  !For Radon
- !if (mu_erf .lt. 40) then
- ! mu_erf += 4d0
- !elseif (mu_erf .lt. 160) then
- ! mu_erf+=8.d0
- !elseif (mu_erf .lt. 800) then
- ! mu_erf+=40.d0
- !elseif (mu_erf .lt. 1600) then
- ! mu_erf+=80.0d0
- !elseif (mu_erf .lt. 4800) then
- ! mu_erf+=160.d0
- !else
- ! mu_erf+=320.d0
- !endif
+  if (mu_erf .lt. 40) then
+   mu_erf += 4d0
+  elseif (mu_erf .lt. 160) then
+   mu_erf+=8.d0
+  elseif (mu_erf .lt. 800) then
+   mu_erf+=40.d0
+  elseif (mu_erf .lt. 1600) then
+   mu_erf+=80.0d0
+  elseif (mu_erf .lt. 4800) then
+   mu_erf+=160.d0
+  else
+   mu_erf+=320.d0
+  endif
 
  !For Oganesson
-  if (mu_erf .lt. 80) then
-   mu_erf += 8d0
-  elseif (mu_erf .lt. 320) then
-   mu_erf+=16.d0
-  elseif (mu_erf .lt. 1600) then
-   mu_erf+=80.d0
-  elseif (mu_erf .lt. 3200) then
-   mu_erf+=160.0d0
-  elseif (mu_erf .lt. 9600) then
-   mu_erf+=320.d0
-  else
-   mu_erf+=640.d0
-  endif
+ !if (mu_erf .lt. 80) then
+ ! mu_erf += 8d0
+ !elseif (mu_erf .lt. 320) then
+ ! mu_erf+=16.d0
+ !elseif (mu_erf .lt. 1600) then
+ ! mu_erf+=80.d0
+ !elseif (mu_erf .lt. 3200) then
+ ! mu_erf+=160.0d0
+ !elseif (mu_erf .lt. 9600) then
+ ! mu_erf+=320.d0
+ !else
+ ! mu_erf+=640.d0
+ !endif
   call ezfio_set_ao_two_e_erf_ints_mu_erf(mu_erf)
 
 end
