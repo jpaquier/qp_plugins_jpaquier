c1897bd63c93ce16c88f10842a18f11306499ffe
Julien Paquier <jpaquier@noumea.lct.jussieu.fr>
2019-04-09 13:30:08 +0200

    he two-electrons fock matrix doesn't work yet

---
+++ dev/dirac_ks_rs/dirac_exchange_dhf_mu_of_r.irp.f
@@ -0,0 +1,139 @@
+program dirac_exchange_dhf_mu_of_r
+  BEGIN_DOC
+  ! produce the dirac energy
+  END_DOC
+   call run_exchange_mu_of_r 
+ end
+
+ subroutine run_exchange_mu_of_r
+  BEGIN_DOC
+  ! Gives the energy for a given value of mu_erf
+  END_DOC
+  use bitmasks
+  implicit none 
+  integer :: i,length
+ !Choose Interaction
+  if (dirac_interaction == "Coulomb") then
+   print*,'**********'
+   print*,'Short-range Coulomb interaction'   
+   print*, 'mu_erf =',mu_erf
+   print*, 'dirac_HF_two_electron_C_Exchange_energy=', dirac_C_Exchange_Energy - dirac_HF_two_electron_c_ex_energy
+   open (10, file='exchange_dhf_Z.dat',position ='append') 
+   write(10,*) mu_erf, dirac_C_Exchange_Energy - dirac_HF_two_electron_c_ex_energy
+ else
+   print *,  'Unrecognized dirac_interaction : '//dirac_interaction
+   stop 1
+  endif
+
+ !!! For 2-electrons systems 
+ !For helium
+  if (mu_erf .lt. 1.25) then
+   mu_erf += 0.125d0
+  elseif (mu_erf .lt. 2.5) then
+   mu_erf+=0.25d0
+  elseif (mu_erf .lt. 25) then
+   mu_erf+=1.25d0
+  else 
+   mu_erf+=2.5d0
+  endif
+
+ !For berylium
+ !if (mu_erf .lt. 1.25) then
+ ! mu_erf += 0.125d0
+ !elseif (mu_erf .lt. 2.5) then
+ ! mu_erf+=0.25d0
+ !elseif (mu_erf .lt. 25) then
+ ! mu_erf+=1.25d0
+ !else 
+ ! mu_erf+=5d0
+ !endif
+
+ !For Neon
+ !if (mu_erf .lt. 2.5) then
+ ! mu_erf += 0.25d0
+ !elseif (mu_erf .lt. 10) then
+ ! mu_erf+=0.5d0
+ !elseif (mu_erf .lt. 50) then
+ ! mu_erf+=2.5d0
+ !elseif (mu_erf .lt. 100) then
+ ! mu_erf+=5.0d0
+ !else
+ ! mu_erf+=10.d0
+ !endif
+
+ !For argon
+ !if (mu_erf .lt. 5) then
+ ! mu_erf += 0.5d0
+ !elseif (mu_erf .lt. 20) then
+ ! mu_erf+=1.d0
+ !elseif (mu_erf .lt. 100) then
+ ! mu_erf+=5.d0
+ !elseif (mu_erf .lt. 200) then
+ ! mu_erf+=10.0d0
+ !else
+ ! mu_erf+=20.d0
+ !endif
+
+ !For Krypton
+ !if (mu_erf .lt. 10) then
+ ! mu_erf += 1d0
+ !elseif (mu_erf .lt. 40) then
+ ! mu_erf+=2.d0
+ !elseif (mu_erf .lt. 200) then
+ ! mu_erf+=10.d0
+ !elseif (mu_erf .lt. 400) then
+ ! mu_erf+=20.0d0
+ !elseif (mu_erf .lt. 1200) then
+ ! mu_erf+=40.d0
+ !else 
+ ! mu_erf+=80.d0
+ !endif
+
+ !For Xenon
+ !if (mu_erf .lt. 20) then
+ ! mu_erf += 2d0
+ !elseif (mu_erf .lt. 80) then
+ ! mu_erf+=4.d0
+ !elseif (mu_erf .lt. 400) then
+ ! mu_erf+=20.d0
+ !elseif (mu_erf .lt. 800) then
+ ! mu_erf+=40.0d0
+ !elseif (mu_erf .lt. 2400) then
+ ! mu_erf+=80.d0
+ !else
+ ! mu_erf+=160.d0
+ !endif
+
+ !For Radon
+ !if (mu_erf .lt. 40) then
+ ! mu_erf += 4d0
+ !elseif (mu_erf .lt. 160) then
+ ! mu_erf+=8.d0
+ !elseif (mu_erf .lt. 800) then
+ ! mu_erf+=40.d0
+ !elseif (mu_erf .lt. 1600) then
+ ! mu_erf+=80.0d0
+ !elseif (mu_erf .lt. 4800) then
+ ! mu_erf+=160.d0
+ !else
+ ! mu_erf+=320.d0
+ !endif
+
+ !For Oganesson
+ !if (mu_erf .lt. 80) then
+ ! mu_erf += 8d0
+ !elseif (mu_erf .lt. 320) then
+ ! mu_erf+=16.d0
+ !elseif (mu_erf .lt. 1600) then
+ ! mu_erf+=80.d0
+ !elseif (mu_erf .lt. 3200) then
+ ! mu_erf+=160.0d0
+ !elseif (mu_erf .lt. 9600) then
+ ! mu_erf+=320.d0
+ !else
+ ! mu_erf+=640.d0
+ !endif
+ 
+
+end
+
--- dev/dirac_ks_rs/dirac_ks_rs.main.irp.f
+++ dev/dirac_ks_rs/dirac_ks_rs.main.irp.f
@@ -9,49 +9,55 @@ program dirac_ks_rs
  double precision :: dm(N_states),tr_dm(N_states),tr_gamma_2(N_states),rho(N_states),rho_lda(N_states)
  complex*16 :: large_aos_array(large_ao_num),large_aos_array_bis(large_ao_num)
  complex*16 :: small_aos_array(small_ao_num),small_aos_array_bis(small_ao_num),u_dotc_v
- f16 = 0.16666666666666666d0
- f13 = 0.3333333333333333d0
- f23 = 0.6666666666666667d0
- f43 = 1.3333333333333333d0
- ckf = 3.0936677262801355d0
- c = speed_of_light
- kF_moyen = 0.d0
- kF_bis_moyen = 0.d0
- n_moyen = 0.d0
- do istate = 1, N_states
-  do i = 1, n_points_final_grid
-   r(1) = final_grid_points(1,i)
-   r(2) = final_grid_points(2,i)
-   r(3) = final_grid_points(3,i)
-   weight=final_weight_at_r_vector(i)
-   rho(istate) = dirac_one_body_dm_at_r(i,istate)
-   kF(istate) = ckf*(rho(istate)**f13)
-   tr_gamma_2(istate) = dirac_one_body_tr_dm_2_at_r(i,istate)
-   call dirac_ex_LDA_sr(mu_erf,rho(istate),tr_gamma_2(istate),e_x,v_x)
-   rho_lda(istate) = dsqrt(2.d0*tr_gamma_2(istate))
-   kF_bis(istate) = ckf*(rho_lda(istate)**f13)
-   tmp_c(istate) = c/kF_bis(istate)
-   if (tr_gamma_2(istate) .gt. 1d-5) then
-    do j = 1, 4
-     tmp_kF = 4.375106855981304d0*(rho_lda*dsqrt(-1.d0/(-4.d0 - 9.d0*tmp_c**2 - 9.d0*tmp_c**4 + 9.d0*tmp_c**4*dlog(dsqrt(1.d0 + tmp_c**(-2)) + 1.d0/tmp_c)* (2.d0*dsqrt(1.d0 + tmp_c**2) - tmp_c**2*dlog(dsqrt(1.d0 + tmp_c**(-2)) + 1.d0/tmp_c)))))**f13
-     tmp_c = c/tmp_kF
-    enddo
-   else
-    tmp_kF = kF_bis
-   endif
-   n_moyen (istate) += weight*rho(istate)
-   kF_moyen(istate) += weight*0.0337737d0* kF(istate)**4  
-   kF_bis_moyen(istate) += weight*0.0337737d0* tmp_kF(istate)**4
-   
-   open (10, file='exchange_energy_density')
-   write(10,*) r(1),r(2),r(3), tmp_kF(istate),e_x
-  enddo
-  open (11, file='kF_moyen')
-  write(11,*) "elec_num =", n_moyen(istate)
-  write(11,*) "kF_moyen =", kF_moyen(istate), kF_moyen(istate)/n_moyen(istate)
-  write(11,*) "kF_moyen_bis=", kF_bis_moyen(istate), kF_bis_moyen(istate)/n_moyen(istate)
- enddo
+!f16 = 0.16666666666666666d0
+!f13 = 0.3333333333333333d0
+!f23 = 0.6666666666666667d0
+!f43 = 1.3333333333333333d0
+!ckf = 3.0936677262801355d0
+!c = speed_of_light
+!kF_moyen = 0.d0
+!kF_bis_moyen = 0.d0
+!n_moyen = 0.d0
+!do istate = 1, N_states
+! do i = 1, n_points_final_grid
+!  r(1) = final_grid_points(1,i)
+!  r(2) = final_grid_points(2,i)
+!  r(3) = final_grid_points(3,i)
+!  weight=final_weight_at_r_vector(i)
+!  rho(istate) = dirac_one_body_dm_at_r(i,istate)
+!  kF(istate) = ckf*(rho(istate)**f13)
+!  tr_gamma_2(istate) = dirac_one_body_tr_dm_2_at_r(i,istate)
+!  call dirac_ex_LDA_sr(mu_erf,rho(istate),tr_gamma_2(istate),e_x,v_x)
+!  rho_lda(istate) = dsqrt(2.d0*tr_gamma_2(istate))
+!  kF_bis(istate) = ckf*(rho_lda(istate)**f13)
+!  tmp_c(istate) = c/kF_bis(istate)
+!  if (tr_gamma_2(istate) .gt. 1d-5) then
+!   do j = 1, 4
+!    tmp_kF = 4.375106855981304d0*(rho_lda*dsqrt(-1.d0/(-4.d0 - 9.d0*tmp_c**2 - 9.d0*tmp_c**4 + 9.d0*tmp_c**4*dlog(dsqrt(1.d0 + tmp_c**(-2)) + 1.d0/tmp_c)* (2.d0*dsqrt(1.d0 + tmp_c**2) - tmp_c**2*dlog(dsqrt(1.d0 + tmp_c**(-2)) + 1.d0/tmp_c)))))**f13
+!    tmp_c = c/tmp_kF
+!   enddo
+!  else
+!   tmp_kF = kF_bis
+!  endif
+!  n_moyen (istate) += weight*rho(istate)
+!  kF_moyen(istate) += weight*0.0337737d0* kF(istate)**4  
+!  kF_bis_moyen(istate) += weight*0.0337737d0* tmp_kF(istate)**4
+!  
+!  open (10, file='exchange_energy_density')
+!  write(10,*) r(1),r(2),r(3), tmp_kF(istate),e_x
+! enddo
+! open (11, file='kF_moyen')
+! write(11,*) "elec_num =", n_moyen(istate)
+! write(11,*) "kF_moyen =", kF_moyen(istate), kF_moyen(istate)/n_moyen(istate)
+! write(11,*) "kF_moyen_bis=", kF_bis_moyen(istate), kF_bis_moyen(istate)/n_moyen(istate)
+!enddo
 
+ do j = 1,1
+  do i = 1,1
+  !print*,i,j,dirac_HF_two_electron_c_ex_int_mu_of_r(i,j)
+   print*,i,j,dirac_ao_bi_elec_C_Exchange_integral(i,j)
+  enddo
+ enddo
 
 
 end
